513fb1188be72895ac1f2c976b0ce426e8318dcd
Julian <julianjometzger@gmail.com>
2024-12-09 17:35:01 +0100

    feat: wip backsubst.c

--- src/backsubst.c
+++ src/backsubst.c
@@ -6,17 +6,6 @@
  */
 int backsubst(Matrix *x, Matrix *mat, Matrix *b)
 {
-	/**
-	 * Tutaj należy umieścić właściwą implemntację.
-	 */
-
-	/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-	int i;
-	for (i = 0; i < x->r; i++)
-	{
-		x->data[i][0] = b->data[i][0];
-	}
 
 	return 0;
 }
